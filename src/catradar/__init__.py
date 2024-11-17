a584b369f178497a71e4368b2116513af6933d38
Artem Demchenko <artdem243@gmail.com>
2024-11-17 23:01:24 +0300

    fix: Reformat

--- src/catradar/__init__.py
+++ src/catradar/__init__.py
@@ -2,7 +2,7 @@ import os
 
 import taichi as ti
 
-if os.environ.get('GITHUB_ACTIONS') == 'true':
+if os.environ.get("GITHUB_ACTIONS") == "true":
     print("Testing mode, running on the cpu")
     ti.init(arch=ti.cpu, debug=True)
 else:
