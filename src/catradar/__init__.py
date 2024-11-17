9e905aa9f9dbf72b64c627c750e8acd3ff4d2d73
Artem Demchenko <artdem243@gmail.com>
2024-11-17 23:00:30 +0300

    ci: Add flag `debug=True`

--- src/catradar/__init__.py
+++ src/catradar/__init__.py
@@ -4,7 +4,7 @@ import taichi as ti
 
 if os.environ.get('GITHUB_ACTIONS') == 'true':
     print("Testing mode, running on the cpu")
-    ti.init(arch=ti.cpu)
+    ti.init(arch=ti.cpu, debug=True)
 else:
     print("Regular mode, running on the gpu")
     ti.init(arch=ti.gpu)
