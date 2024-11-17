13e06f6e256d5bede28fb42a6d23df79d4616648
Artem Demchenko <artdem243@gmail.com>
2024-11-18 00:37:23 +0300

    fix: grid_manager

--- src/catradar/grid_manager.py
+++ src/catradar/grid_manager.py
@@ -183,7 +183,7 @@ def compute_states(
 
                                 if intersect_len == INTERSECTION_NUM:
                                     break  # Exit early for performance
-                        elif dist <= R1:
+                        elif dist <= R1 and state != STATE_INTERSECTION:
                             temp = dist - R0 * 0.75
                             prob = 1.0 / (temp * temp)
                             if ti.random() < prob:
