ab55eeb497a326ad2cca1b5ca19d997fe4786659
Artem Demchenko <artdem243@gmail.com>
2024-11-17 22:29:33 +0300

    tests: Introduce tests for `grid_manager`

---
+++ tests/catradar/test_grid_manager.py
@@ -0,0 +1,96 @@
+import pytest
+import taichi as ti
+
+from catradar.common import STATE_INTERSECTION, STATE_IDLE
+from catradar.grid_manager import _calc_dist, setup_grid_data, compute_states
+
+
+# Compute only STATE_INTERSECTION states because state STATE_INTERACT is randomized and will not be tested
+@ti.kernel
+def naive_algo(
+    N: ti.i32,
+    RO: ti.i32,
+    positions: ti.template(),
+    states: ti.template(),
+    norm_func: ti.i32,
+):
+    for i in range(N):
+        states[i] = STATE_IDLE
+
+    for i in range(N):
+        for j in range(i + 1, N):
+            if _calc_dist(positions[i], positions[j], norm_func) <= RO:
+                states[i] = STATE_INTERSECTION
+                states[j] = STATE_INTERSECTION
+
+
+# copy-pasted from positions_updater.initialize_positions
+@ti.kernel
+def initialize_test_positions(
+    N: ti.i32, X: ti.i32, Y: ti.i32, positions: ti.template(), opt: ti.i32
+):
+    if opt == 0:
+        for i in range(N):
+            positions[i] = ti.Vector([50 + ti.random() * 10, 50 + ti.random()])
+    if opt == 1:
+        for i in range(N):
+            positions[i] = ti.Vector([ti.random() * X, ti.random() * Y])
+
+
+THRESHOLD = 0.05
+
+
+@pytest.mark.parametrize(
+    "N,X,Y,R0,R1,LIMIT_PER_CELL,INTERSECTION_NUM,update_intersections",
+    [
+        pytest.param(100, 1000, 1000, 5, 10, 250, 10, True),  # small grid
+        pytest.param(500, 1000, 1000, 10, 20, 250, 10, True),  # normal grid
+        pytest.param(
+            10000, 1000, 1000, 10, 20, 250, 10, True
+        ),  # normal grid with many cats
+        pytest.param(
+            10000, 10000, 100000, 25, 50, 250, 10, True
+        ),  # big grid with many cats
+        pytest.param(
+            5000, 10000, 10000, 5, 20, 250, 10, False
+        ),  # regular grid with false as update_intersections
+    ],
+)
+def test_compute_states(
+    N, X, Y, R0, R1, LIMIT_PER_CELL, INTERSECTION_NUM, update_intersections
+):
+    positions = ti.Vector.field(2, dtype=ti.f32, shape=N)
+    states_expected = ti.field(dtype=ti.i32, shape=N)
+    states_actual = ti.field(dtype=ti.i32, shape=N)
+
+    intersections_mock = ti.field(
+        dtype=ti.i32,
+        shape=(N, INTERSECTION_NUM + 1),
+    )
+    logged_id_mock = 0
+
+    wrong_count = 0
+    for init_opt in range(2):
+        initialize_test_positions(N, X, Y, positions, init_opt)
+        for norm_func in range(3):
+            naive_algo(N, R0, positions, states_expected, norm_func)
+
+            setup_grid_data(X, Y, N, R0, R1, LIMIT_PER_CELL, INTERSECTION_NUM)
+            compute_states(
+                positions,
+                states_actual,
+                intersections_mock,
+                update_intersections,
+                norm_func,
+                logged_id_mock,
+            )
+
+            for i in range(N):
+                if states_expected[i] == STATE_INTERSECTION:
+                    if states_actual[i] != STATE_INTERSECTION:
+                        wrong_count += 1
+                if states_expected[i] == STATE_IDLE:
+                    if states_actual[i] == STATE_INTERSECTION:
+                        wrong_count += 1
+
+    assert wrong_count <= int(THRESHOLD * N)
